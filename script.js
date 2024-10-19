a8cd8ea2fcc07d10aa4e44678afd092e470ebbd9
LLoydowski <janekbaszczynski@gmail.com>
2024-10-19 19:51:18 +0200

    finished

--- script.js
+++ script.js
@@ -7,23 +7,83 @@ const movieNames = {
     6: "Revenge of the Sith"
 }
 
-document.getElementById("personSearch").addEventListener("keyup", (e) => {
+let fetchType = "person"
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+document.getElementById("search").addEventListener("keyup", (e) => {
     if(e.key == "Enter"){
-        fetchAndSearchPeople()
+        fetchData()
     }
 })
 
+document.getElementById("mobileBtn").addEventListener("click", (e) => {
+    document.querySelector(".mobileBg").style.display = "flex"
+})
+document.querySelector(".mobileBg").addEventListener("click", async () => {
+    await sleep(10)
+    document.querySelector(".mobileBg").style.display = "none"
+})
+
+function fetchData(){
+    switch(fetchType){
+        case "person":
+            fetchAndSearchPeople()
+            break
+        case "starship":
+            fetchAndSearchStarship()
+            break
+    }
+}
+
+function switchView(view){
+    document.querySelectorAll(".card").forEach(card => card.classList.add("hidden"))
+    document.querySelectorAll(".poster").forEach(poster => poster.classList.add("disabled"))
+
+
+    switch(view){
+        case "person":
+            document.querySelector(".person").classList.remove("hidden")
+            fetchType = "person"
+
+            document.getElementById("starshipModel").innerHTML = 'Model: '
+            document.getElementById("starshipLength").innerHTML = 'Length: '
+            document.getElementById("pilots").children[1].innerHTML = ''
+            document.getElementById("starshipAppearances").children[1].innerHTML = ''
+
+            break;
+        case "starship":
+            document.querySelector(".starship").classList.remove("hidden")
+            fetchType = "starship"
+
+            document.getElementById("sex").innerHTML = 'Sex: '
+            document.getElementById("personHeight").innerHTML = 'Height: '
+            document.getElementById("personWeight").innerHTML = 'Weight: '
+            document.getElementById("vehicles").children[1].innerHTML = ''
+            document.getElementById("personAppearances").children[1].innerHTML = ''
+
+            break;
+    }
+}
+
+
 async function fetchAndSearchPeople(){
     let loadingScreen = document.getElementById("loadingScreen")
-
+    
     loadingScreen.classList.remove("disabled")
 
+    let personNameInput = document.getElementById("search")
+    let personName = personNameInput.value 
+    
+    personNameInput.blur()
+    
     let response = await fetch("https://swapi.dev/api/people")
     .then(response => response.json())
     response = await Promise.resolve(response)
     let pages = Math.floor(response.count / 10) + 1
 
-    let personName = document.getElementById("personSearch").value
 
 
     for(let i = 1; i <= pages; i++){
@@ -43,6 +103,7 @@ async function fetchAndSearchPeople(){
         }
 
         if(person){
+            document.getElementById("search").value = ''
             // Name
 
             document.getElementById("personName").textContent = person.name
@@ -110,6 +171,116 @@ async function fetchAndSearchPeople(){
                 vehiclesDiv.innerHTML += `<li>${element}</li>`
             })
 
+            break
+        }
+        if(i+1 > pages && !person){
+            alert("Character wasn't found")
+            break
+        }
+    }
+
+    loadingScreen.classList.add("disabled")
+}
+
+async function fetchAndSearchStarship(){
+    let loadingScreen = document.getElementById("loadingScreen")
+    
+    loadingScreen.classList.remove("disabled")
+
+    let starshipNameInput = document.getElementById("search")
+    let starshipName = starshipNameInput.value 
+    
+    starshipNameInput.blur()
+    
+    let response = await fetch("https://swapi.dev/api/starships")
+    .then(response => response.json())
+    response = await Promise.resolve(response)
+    let pages = Math.floor(response.count / 10) + 1
+
+
+
+    for(let i = 1; i <= pages; i++){
+        let page = await Promise.resolve(fetch(`https://swapi.dev/api/starships/?page=${i}`)
+        .then(response => response.json()))
+
+        let starships = page.results
+
+        let starship
+
+        for(let j = 0; j < starships.length; j++){
+            if(starships[j].name == starshipName){
+                console.log("Found")
+                starship = starships[j]
+                break
+            }
+        }
+
+        if(starship){
+            document.getElementById("search").value = ''
+            // Name
+
+            document.getElementById("starshipName").textContent = starship.name
+
+            // Basic Info
+
+            document.getElementById("starshipModel").textContent = `Model: ${starship.model}`
+            document.getElementById("starshipLength").textContent = `Length: ${starship.length} m`
+
+            // Posters
+
+            let posters = Array.from(document.querySelectorAll(".poster"))
+
+            let moviesLinks = starship.films
+            let movies = []
+            
+            moviesLinks.forEach(link => {
+                movies.push(link[link.length - 2])
+            })
+            
+            posters.forEach(poster => {
+                poster.classList.add("disabled")
+                if(movies.includes(poster.getAttribute("posterID"))){
+                    poster.classList.remove("disabled")
+                }
+            })
+            
+            // Appearances
+
+            let appearances = document.getElementById("starshipAppearances").children[1]
+
+            appearances.innerHTML = ""
+
+            movies.forEach(movie => {
+                appearances.innerHTML += `<li>${movieNames[movie]}</li>`
+            })
+
+
+            // Pilots
+
+            let pilotsDiv = document.getElementById("pilots").children[1]
+
+            pilotsDiv.innerHTML = ""
+
+            let pilotsList = []
+
+            for(let j = 0; j < starship.pilots.length; j++){
+                let data = await fetch(starship.pilots[j])
+                let json = await data.json()
+                let name = await json.name
+                console.log(name)
+                pilotsList.push(name)
+            }
+
+            pilotsList.forEach(element => {
+                pilotsDiv.innerHTML += `<li>${element}</li>`
+            })
+            if(pilotsList.length == 0){
+                pilotsDiv.innerHTML += `<li>This starship had no pilots!</li>`
+            }
+            break
+        }
+        if(i+1 > pages && !starship){
+            alert("Starship wasn't found")
             break
         }
     }
